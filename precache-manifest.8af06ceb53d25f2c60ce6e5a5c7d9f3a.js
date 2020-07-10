da9a02684c5ed328c473fcbb1c900aefba9088b1
Milosm991 <milosmanojlovic91@gmail.com>
2020-07-10 16:37:00 +0200

    Updates

---
+++ precache-manifest.8af06ceb53d25f2c60ce6e5a5c7d9f3a.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "296fd5032e85deeadb6a79665eec19f4",
+    "url": "/final-project/index.html"
+  },
+  {
+    "revision": "87058ad5c6b2bc1ed3a5",
+    "url": "/final-project/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "bd03e53dfb63a5ba5237",
+    "url": "/final-project/static/css/main.3647e755.chunk.css"
+  },
+  {
+    "revision": "87058ad5c6b2bc1ed3a5",
+    "url": "/final-project/static/js/2.5255d2a3.chunk.js"
+  },
+  {
+    "revision": "cb4293a028d0175415ddb31dc1aaaefb",
+    "url": "/final-project/static/js/2.5255d2a3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bd03e53dfb63a5ba5237",
+    "url": "/final-project/static/js/main.d49450c2.chunk.js"
+  },
+  {
+    "revision": "62c7ebc25974f2470bb2",
+    "url": "/final-project/static/js/runtime-main.f9b32987.js"
+  },
+  {
+    "revision": "6cfcc6be4fcc2243d131f5baa514cb94",
+    "url": "/final-project/static/media/not-found.6cfcc6be.png"
+  }
+]);
